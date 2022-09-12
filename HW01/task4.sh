9ffc935995f9cc28f38c264a01655fe2a88a4f74
Jackson Fox <jmfox8@wisc.edu>
2022-09-12 13:44:57 -0500

    wrote code for task 4

---
+++ HW01/task4.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+#SBATCH --job-name=FirstSlurm
+#SBATCH --partition=wacc
+#SBATCH --ntasks=1  --cpus-per-task=2
+#SBATCH --time=0-00:10:00
+#SBATCH --output="FirstSlurm.out"
+#SBATCH --
