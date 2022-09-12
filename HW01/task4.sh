4a2c30b0865fc5df79fed67c33d4583a31bf3c94
Jackson Fox <jmfox8@wisc.edu>
2022-09-12 14:00:26 -0500

    More changes to task4

--- HW01/task4.sh
+++ HW01/task4.sh
@@ -4,4 +4,7 @@
 #SBATCH --ntasks=1  --cpus-per-task=2
 #SBATCH --time=0-00:10:00
 #SBATCH --output="FirstSlurm.out"
-#SBATCH --
+#SBATCH --error="FirstSlurm.err"
+
+cd $SLURM_SUBMIT_DIR
+echo $(hostname)
