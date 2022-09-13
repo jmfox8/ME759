f83314f1adad5626603441d6905d458c427b4e72
Jackson Fox <jmfox8@wisc.edu>
2022-09-13 12:34:45 -0500

    added cout method

--- HW01/task6.cpp
+++ HW01/task6.cpp
@@ -1,18 +1,19 @@
-#include  <iostream>
+#include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
 
-int N = atoi(argv[1]);
+    int N = atoi(argv[1]);
 
-for(int i = 0; i<=N; i++){
-printf("%d ",i);
-}
-printf("\n");
-for(int i = N; i>=0; i--){
-
-    printf("%d ",i);
-}
-printf("\n");
+    for (int i = 0; i <= N; i++)
+    {
+        printf("%d ", i);
+    }
+    printf("\n");
+    for (int i = N; i >= 0; i--)
+    {
+        std::cout << i << " ";
+    }
+    cout << "\n";
 }
