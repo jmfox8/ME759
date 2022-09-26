2d0f8ecf06ff28020a284573ee97855b6b1c684c
Jackson Fox <jmfox8@wisc.edu>
2022-09-26 12:19:44 -0500

    Corrected code for reference - POST GRADING

--- HW02/scan.cpp
+++ HW02/scan.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 void scan(const float *arr, float *output, std::size_t n){
     output[0] = arr[0];
-    for (int i = 1; i<n+1; i++){
+    for (int i = 1; i<n-1; i++){
 
         output[i] = output[i-1]+ arr[i];
     }
