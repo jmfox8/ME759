986b63ad08fcad526adc57613f50ec448c9bfbe2
Jackson Fox <jmfox8@wisc.edu>
2022-09-26 20:50:47 -0500

    fixed code

--- HW02/scan.cpp
+++ HW02/scan.cpp
@@ -6,8 +6,12 @@ using namespace std;
 
 void scan(const float *arr, float *output, std::size_t n){
     output[0] = arr[0];
-    for (int i = 1; i<n-1; i++){
-
+    if (n == 0){
+    
+    }
+    else {
+        for (int i = 1; i<n; i++){
         output[i] = output[i-1]+ arr[i];
+    }
     }
 }
