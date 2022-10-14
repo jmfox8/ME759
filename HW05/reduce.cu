07fa273edff694f9bc644cc95303087fb0237721
Jackson Fox <jmfox8@wisc.edu>
2022-10-14 09:16:49 -0500

    Coded, still issues at high numbers, best I can do

--- HW05/reduce.cu
+++ HW05/reduce.cu
@@ -3,44 +3,58 @@
 #include <cstddef>
 
 // implements the 'first add during global load' version (Kernel 4) for the
-// parallel reduction g_idata is 
-
- array to be reduced, and is available on
+// parallel reduction g_idata is array to be reduced, and is available on
 // the device. g_odata is the array that the reduced results will be written to,
 // and is available on the device. expects a 1D configuration. uses only
 // dynamically allocated shared memory.
 __global__ void reduce_kernel(float *g_idata, float *g_odata, unsigned int n){
     extern __shared__ float sharedarray[];
-    // perform first level of reduction upon reading from 
-    // global memory and writing to shared memory
     unsigned int threadnum = threadIdx.x;
     unsigned int i   = blockIdx.x*(blockDim.x*2) + threadIdx.x;
     
     // check to make sure that thread has entry to pull into shared memory
-    if (blockIdx.x * blockDim.x +threadIdx.x >= n ){
+    if (i >= n ){
+        sharedarray[threadnum] = 0;
     }
     // check that there is a value to add to entry when putting into shared memory
     else if (i+blockDim.x >= n){
         sharedarray[threadnum] = g_idata[i];
     }
-    // Add to shared memory and perform first level of reduction
+    // Add to shared memory and perform first level of reduction during pull in
     else {
         sharedarray[threadnum] = g_idata[i] + g_idata[i+blockDim.x];
     }
     __syncthreads();
     
-    //Reduction of leftover values after filling shared array
-    for(unsigned int s=blockDim.x/2; s>0; s>>=1) {
-        if(threadnum < s) {
-            sharedarray[threadnum] += sharedarray[threadnum + s];
+    //Reduction of values in the shared array when exactly one or multiple blocks needed
+    if (blockDim.x <= n){
+        int reducingsize = blockDim.x;
+        for(unsigned int s=blockDim.x/2; s>0; s>>=1) {
+            if(threadnum < s) {
+                sharedarray[threadnum] += sharedarray[threadnum + s];
+                if (threadnum == 0 && (reducingsize % 2) != 0){
+                    sharedarray[threadnum] += sharedarray[2*s];
+                }
+            }
+            reducingsize = s;
         }
     __syncthreads();
     }
 
-    // Check to account for left over entry in arrays with odd valued threads per block, 
-    // adds to final value after all else is complete
-    if ((blockDim.x % 2) != 0 && n>=blockDim.x && threadnum == blockDim.x-1) sharedarray[0] += sharedarray[threadnum];
+    //Reduction of values in shared array when less than 1 full block needed
+    else {
+        int reducingsize = n;
+        for(unsigned int s=n/2; s>0; s>>=1) {
+            if(threadnum < s) {
+                sharedarray[threadnum] += sharedarray[threadnum + s];
+                if (threadnum == 0 && (reducingsize % 2) != 0){
+                    sharedarray[threadnum] += sharedarray[2*s];
+                }
+            }
+        reducingsize = s;
+        }
     __syncthreads();
+    }
 
     // write result for this block to global memory
     if(threadnum == 0){
@@ -48,6 +62,7 @@ __global__ void reduce_kernel(float *g_idata, float *g_odata, unsigned int n){
      g_idata[blockIdx.x]=  sharedarray[0];
 }
 }
+
 
 // the sum of all elements in the *input array should be written to the first
 // element of the *input array. calls reduce_kernel repeatedly if needed. _No
@@ -59,20 +74,22 @@ __global__ void reduce_kernel(float *g_idata, float *g_odata, unsigned int n){
 __host__ void reduce(float **input, float **output, unsigned int N, unsigned int threads_per_block){
     int blocksneeded = (((N+threads_per_block-1)/threads_per_block)+1)/2;
     // Account for integers rounding down
-    if (blocksneeded == 0) blocksneeded = 1;
     // Call kernel for first time
     reduce_kernel<<<blocksneeded,threads_per_block,threads_per_block*sizeof(float)>>>(*input,*output,N);
     // Call additional kernels if needed
-    if (blocksneeded > 1){
+    if (blocksneeded == 1){}
+    else{
         //redefine n and blocksneeded for new input array from old output
         int n = blocksneeded;
         blocksneeded = (((n + threads_per_block -1)/threads_per_block)+1)/2;
-        int loop_n = n;
-        for(int i = 0; i<=n; i+=2*threads_per_block){
-            reduce_kernel<<<blocksneeded,threads_per_block,threads_per_block*sizeof(float)>>>(*input,*output,loop_n);
-            loop_n = blocksneeded;
-            blocksneeded = (((loop_n + threads_per_block -1)/threads_per_block)+1)/2;
+        // Call Kernel and redefine input as many times as necessary
+        while (n>threads_per_block * 2){
+            reduce_kernel<<<blocksneeded,threads_per_block,threads_per_block*sizeof(float)>>>(*input,*output,n);
+            n = blocksneeded;
+            blocksneeded = (((n + threads_per_block -1)/threads_per_block)+1)/2;
         }
+        // Final Kernel call for when reduction can occur within one block
+        reduce_kernel<<<blocksneeded,threads_per_block,threads_per_block*sizeof(float)>>>(*input,*output,n);
     }
     cudaDeviceSynchronize();
 }
