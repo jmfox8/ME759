3e275100c26bc25d248c054f660cf3fb1c9d297c
Jackson Fox <jmfox8@wisc.edu>
2022-10-26 09:24:55 -0500

    cleaned up cub script

--- HW07/task1_cub.cu
+++ HW07/task1_cub.cu
@@ -1,5 +1,6 @@
-#define CUB_STDERR // print CUDA runtime errors to console
 #include <stdio.h>
+#include <stdlib.h>
+#include <random>
 #include <cub/util_allocator.cuh>
 #include <cub/device/device_reduce.cuh>
 #include "cub/util_debug.cuh"
@@ -13,7 +14,8 @@ int main(int argc, char *argv[]) {
     
     // Set up host arrays
     float h_in[n];
-
+    float sum = 0;
+    
     // Initialization for timing
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
@@ -27,40 +29,41 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i<n; i++){
         h_in[i] = RD(generator);
         }
-
+    
     // Set up device arrays
     float* d_in = NULL;
-    CubDebugExit(g_allocator.DeviceAllocate((void**)& d_in, sizeof(float) * n));
+    (g_allocator.DeviceAllocate((void**)& d_in, sizeof(float) * n));
     // Initialize device input
-    CubDebugExit(cudaMemcpy(d_in, h_in, sizeof(float) * n, cudaMemcpyHostToDevice));
+    cudaMemcpy(d_in, h_in, sizeof(float) * n, cudaMemcpyHostToDevice);
     // Setup device output array
     float* d_sum = NULL;
-    CubDebugExit(g_allocator.DeviceAllocate((void**)& d_sum, sizeof(float) * 1));
+    g_allocator.DeviceAllocate((void**)& d_sum, sizeof(float) * 1);
+    
     // Request and allocate temporary storage
     void* d_temp_storage = NULL;
     size_t temp_storage_bytes = 0;
-    CubDebugExit(DeviceReduce::Sum(d_temp_storage, temp_storage_bytes, d_in, d_sum, n));
-    CubDebugExit(g_allocator.DeviceAllocate(&d_temp_storage, temp_storage_bytes));
+    DeviceReduce::Sum(d_temp_storage, temp_storage_bytes, d_in, d_sum, n);
+    g_allocator.DeviceAllocate(&d_temp_storage, temp_storage_bytes);
 
     // Do and time the actual reduce operation
     cudaEventRecord(start);
-    CubDebugExit(DeviceReduce::Sum(d_temp_storage, temp_storage_bytes, d_in, d_sum, n));
+    DeviceReduce::Sum(d_temp_storage, temp_storage_bytes, d_in, d_sum, n);
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
-    int gpu_sum;
-    CubDebugExit(cudaMemcpy(&gpu_sum, d_sum, sizeof(float) * 1, cudaMemcpyDeviceToHost));
-    // Check for correctness
-    printf("\t%s\n", (gpu_sum == sum ? "Test passed." : "Test falied."));
-    printf("\tSum is: %d\n", gpu_sum);
+    float gpu_sum;
+    cudaMemcpy(&gpu_sum, d_sum, sizeof(float) * 1, cudaMemcpyDeviceToHost);
 
     // Get elapsed time
     float ms;
     cudaEventElapsedTime(&ms, start, stop);
     
+    // Print outputs
+    printf("%f \n%f \n",gpu_sum,ms);
+
     // Cleanup
-    if (d_in) CubDebugExit(g_allocator.DeviceFree(d_in));
-    if (d_sum) CubDebugExit(g_allocator.DeviceFree(d_sum));
-    if (d_temp_storage) CubDebugExit(g_allocator.DeviceFree(d_temp_storage));
+    if (d_in) g_allocator.DeviceFree(d_in);
+    if (d_sum) g_allocator.DeviceFree(d_sum);
+    if (d_temp_storage) g_allocator.DeviceFree(d_temp_storage);
     
     return 0;
 }
