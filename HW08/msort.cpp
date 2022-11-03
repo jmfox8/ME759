3c7a389a500d24dd699784ec936aca6c414881f1
Jackson Fox <jmfox8@wisc.edu>
2022-11-03 01:01:39 -0500

    general updates

--- HW08/msort.cpp
+++ HW08/msort.cpp
@@ -2,6 +2,12 @@
 #include <cstddef>
 #include <omp.h>
 
+
+void copyarray(int* arr, int* copy, const std::size_t n);
+void merge(int* arr, int ileft, int iright, int iend, int* copy);
+void serialsort(int* arr, const std::size_t n);
+
+
 // This function does a merge sort on the input array "arr" of length n. 
 // You can add more functions as needed to complete the merge sort,
 // but do not change this file. Declare and define your addtional
@@ -13,6 +19,36 @@
 // the threshold, a serial sort algorithm will be used to avoid overhead
 // of task scheduling
 
+void copyarray(int* arr, int* copy, const std::size_t n){
+    for (int i = 0; i < n; i++){
+        copy[i] = arr[i];
+    }
+}
+void merge(int* arr, int ileft, int iright, int iend, int* copy){
+    int i = ileft;
+    int j = iright;
+    for (int k = ileft; k< iend; k++){
+        if (i < iright && (j >= iend || arr[i] <= arr[j])){
+            copy[k] = arr[i];
+            i += 1;
+        }
+        else{
+            copy[k] = arr[j];
+            j+= 1;
+        }
+    }
+}
+
+void serialsort(int* arr, const std::size_t n){
+    int* arr2  = new int[n];
+    for (int width = 1; width<n; width = 2*width){
+        for (int i = 0; i < n; i = i + 2*width){
+            merge(arr, i, std::min(i+width, n),min(i+2*width,n),arr2);
+        }
+        copyarray(arr2, arr, n);
+    }
+}
+
 void msort(int* arr, const std::size_t n, const std::size_t threshold){
-    
+    if (n<threshold) serialsort(arr,n);
 }
---
+++ HW08/task3.cpp
@@ -0,0 +1,55 @@
+#include "msort.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <math.h>
+#include <random>
+#include <vector>
+#include <chrono>
+#include <omp.h>
+
+int main(int argc, char *argv[]){
+    // Get Command Line Input
+    int n = atoi(argv[1]);
+    int t = atoi(argv[2]);
+    int ts = atoi(argv[3]);
+    if (t < 1 || t > 20){
+        std::cout<<"Thread count out of bounds";
+        exit(0);
+    }
+
+    // Intialize and Allocate Memory for Arrays
+    int* arr = new int[n];
+
+    // Initialization for OpenMP
+    omp_set_num_threads(t); 
+    
+    // Initialization for timing
+    std::chrono::duration<double, std::milli> ms;
+    std::chrono::high_resolution_clock::time_point start;
+    std::chrono::high_resolution_clock::time_point end;
+
+    // Initialization for randomization
+    std::random_device entropy_source;
+    std::mt19937 generator(entropy_source());
+
+    // Generate random float values and populate arrays
+    std::uniform_real_distribution<float> RD(-1000,1000);
+    for (int i = 0; i<n; i++){
+            arr[i] = static_cast<int>(RD(generator));
+        }
+
+    // Execute Function Call with timing
+    start = std::chrono::high_resolution_clock::now();
+    msort(arr, n, ts);
+    end = std::chrono::high_resolution_clock::now();
+    ms = std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(end-start);
+
+    //Print results
+    std::cout << arr[0] << "\n";
+    std::cout << arr[n-1] << "\n";
+    std::cout << ms.count() <<"\n";
+   
+    // Deallocate Memory for Arrays
+    delete[] arr;
+}
