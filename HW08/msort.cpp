2935153c76ffd06f1f1b7a4e0837cc18c38eebcc
Jackson Fox <jmfox8@wisc.edu>
2022-11-04 17:56:23 -0500

    Finished task3

--- HW08/msort.cpp
+++ HW08/msort.cpp
@@ -2,15 +2,13 @@
 #include <cstddef>
 #include <iostream>
 #include <omp.h>
+#include <algorithm>
 
 
 void copyarray(int* arr, int* copy, const std::size_t n);
-void serialmerge(int* arr, int ileft, int iright, int iend, int* copy);
-void serialsort(int* arr, const std::size_t n);
-
 void merge(int* arr, int start, int mid, int end, int* arr2);
-void splitmerge(int* arr2, int start, int end, int* arr);
-void parallelsort(int*arr, const std::size_t n);
+void splitmerge(int* arr2, int start, int end, int* arr, std::size_t threshold);
+void parallelsort(int*arr, const std::size_t n, std::size_t threshold);
 
 
 // This function does a merge sort on the input array "arr" of length n. 
@@ -31,49 +29,21 @@ void copyarray(int* arr, int* copy, const std::size_t n){
     }
 }
 
-// serial Function to re-merge two equally sized arrays in sorted order
-void serialmerge(int* arr, int ileft, int iright, int iend, int* copy){
-    int i = ileft;
-    int j = iright;
-    for (int k = ileft; k< iend; k++){
-        if (i < iright && (j >= iend || arr[i] <= arr[j])){
-            copy[k] = arr[i];
-            i += 1;
-        }
-        else{
-            copy[k] = arr[j];
-            j+= 1;
-        }
-    }
-}
-
-// Serial sorting function - calls copyarray and serialmerge
-void serialsort(int* arr, const std::size_t n){
-    int* arr2  = new int[n];
-    for (int width = 1; width<n; width = 2*width){
-        for (int i = 0; i < n; i = i + 2*width){
-            if (i + width < n){
-                if (i + 2 * width < n) serialmerge(arr, i, i+width, i + 2*width,arr2);
-                else serialmerge(arr,i,i+width,n,arr2);
-            }
-            else serialmerge(arr,i,n,n,arr2);
-        }
-        copyarray(arr2, arr, n);
-    }
-}
-
 //Parallel merging function
 void merge(int* arr, int start, int mid, int end, int* arr2){
     int i = start;
     int j = mid;
-    for (int k = start; k< end; k++){
-        if (i < mid && (j >= end || arr[i] <= arr[j])){
-            arr2[k] = arr[i];
-            i += 1;
-        }
-        else{
-            arr2[k] = arr[j];
-            j += 1;
+    #pragma omp task
+    {
+        for (int k = start; k< end; k++){
+            if (i < mid && (j >= end || arr[i] <= arr[j])){
+                arr2[k] = arr[i];
+                i += 1;
+            }
+            else{
+                arr2[k] = arr[j];
+                j += 1;
+            }
         }
     }
 }
@@ -81,38 +51,36 @@ void merge(int* arr, int start, int mid, int end, int* arr2){
 
 
 // Parallel Split function, recursive
-void splitmerge(int* arr2, int start, int end, int* arr){
-    if (end > start+1){
+void splitmerge(int* arr2, int start, int end, int* arr,std::size_t threshold){
+    if (end - start < threshold) std::sort(arr+start,arr+end);
+        else{
         int split = (start+end)/2;
         #pragma omp task
         {
-            splitmerge(arr,start,split,arr2);
+            splitmerge(arr,start,split,arr2,threshold);
         }
         #pragma omp task
         {
-            splitmerge(arr,split,end,arr2);
+            splitmerge(arr,split,end,arr2,threshold);
         }
-        #pragma omptaskwait
-        #pragma omp critical(RES_lock)
-        {
-            merge(arr2,start, split, end, arr);
-        }
+        #pragma omp taskwait
+        merge(arr2,start, split, end, arr);
     }
 }
 
 // Parallel Sorting Function - calls others
-void parallelsort(int*arr, const std::size_t n){
+void parallelsort(int*arr, const std::size_t n, std::size_t threshold){
     int *arr2 = new int[n];
     copyarray(arr,arr2,n);
     #pragma omp parallel
     #pragma omp single
     {
-    splitmerge(arr2,0,n,arr);
+    splitmerge(arr2,0,n,arr,threshold);
     }
 }
 
 // Main function
 void msort(int* arr, const std::size_t n, const std::size_t threshold){
-    if (n<threshold) serialsort(arr,n);
-    else parallelsort(arr,n);
+    if (n<threshold) std::sort(arr,arr+n);
+    else parallelsort(arr,n,threshold);
 }
